68017b8460988d4c86ab1ee81908d794f972c005
Шлома Сергей Николаевич <-@534school.spb.ru>
2024-01-30 13:34:37 +0300

    edit file main in Seminar9

--- Seminar9_home_task/main.py
+++ Seminar9_home_task/main.py
@@ -6,7 +6,7 @@ random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
 print(data)
-print('')
+print()
 data['tmp'] = 1
 data.set_index([data.index, 'whoAmI'], inplace=True)
 data = data.unstack(level=-1, fill_value = 0).astype(int)
